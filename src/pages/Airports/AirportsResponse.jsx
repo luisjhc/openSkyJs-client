24742a63effd0b5ada5e98eb1c51969eae3dff71
luisjhc <ljoaquincuenca@gmail.com>
2021-07-18 19:45:42 +0200

    adding pagination to the table

--- src/pages/Airports/AirportsResponse.jsx
+++ src/pages/Airports/AirportsResponse.jsx
@@ -1,26 +1,143 @@
 import React from "react";
+import { useTable, usePagination } from "react-table";
+import "./AirportsResponse.css";
 
 export default function AirportsResponse({ listOfArrivals }) {
+  // DEFINING THE DATA WE WANT TO SEE IN THE TABLE 👇
+  const data = React.useMemo(
+    () =>
+      listOfArrivals.map((flight, index) => ({
+        index1: `${flight.estDepartureAirport}`,
+        index2: `${flight.callsign}`,
+        index3: `${flight.estDepartureAirportHorizDistance}`,
+      })),
+
+    [listOfArrivals]
+  );
+
+  // DEFINIG THE COLUMNS OF OUR TABLE 👇
+  const columns = React.useMemo(
+    () => [
+      {
+        Header: "Departure Airport",
+        //accesor is the "key" in the data
+        accessor: "index1",
+      },
+      {
+        Header: "Callsign",
+        accessor: "index2",
+      },
+      {
+        Header: "Departure Airport Distance",
+        accessor: "index3",
+      },
+    ],
+    []
+  );
+
+  // APPLYING THE TABLE INSTANCE TO MARKUP 👇
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    page,
+    prepareRow,
+    canPreviousPage,
+    canNextPage,
+    pageOptions,
+    pageCount,
+    gotoPage,
+    nextPage,
+    previousPage,
+    setPageSize,
+    state: { pageIndex, pageSize },
+  } = useTable(
+    {
+      columns,
+      data,
+      initialState: { pageIndex: 0 },
+    },
+    usePagination
+  );
+
+  // RENDERING THE TABLE 👇
   return (
-    <div>
-      <table>
+    <>
+      <table {...getTableProps()} className="table">
         <thead>
-          <tr>
-            <th>Departure Airport</th>
-            <th>Callsign</th>
-            <th>Departure Airport Distance</th>
-          </tr>
-        </thead>
-        <tbody>
-          {listOfArrivals.map((flight, index) => (
-            <tr key={index}>
-              <td>{flight.estDepartureAirport}</td>
-              <td>{flight.callsign}</td>
-              <td>{flight.estDepartureAirportHorizDistance}</td>
+          {headerGroups.map((headerGroup) => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map((column) => (
+                <th {...column.getHeaderProps()} className="th">
+                  {column.render("Header")}
+                </th>
+              ))}
             </tr>
           ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {page.map((row, i) => {
+            prepareRow(row);
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map((cell) => {
+                  return (
+                    <td {...cell.getCellProps()} className="td">
+                      {cell.render("Cell")}
+                    </td>
+                  );
+                })}
+              </tr>
+            );
+          })}
         </tbody>
       </table>
-    </div>
+
+      {/* PAGINATION STARTS HERE 👇 */}
+      <div className="pagination">
+        <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
+          {"<<"}
+        </button>{" "}
+        <button onClick={() => previousPage()} disabled={!canPreviousPage}>
+          {"<"}
+        </button>{" "}
+        <button onClick={() => nextPage()} disabled={!canNextPage}>
+          {">"}
+        </button>{" "}
+        <button onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>
+          {">>"}
+        </button>{" "}
+        <span>
+          Page{" "}
+          <strong>
+            {pageIndex + 1} of {pageOptions.length}
+          </strong>{" "}
+        </span>
+        <span>
+          | Go to page:{" "}
+          <input
+            type="number"
+            defaultValue={pageIndex + 1}
+            onChange={(e) => {
+              const page = e.target.value ? Number(e.target.value) - 1 : 0;
+              gotoPage(page);
+            }}
+            style={{ width: "100px" }}
+          />
+        </span>{" "}
+        <select
+          value={pageSize}
+          onChange={(e) => {
+            setPageSize(Number(e.target.value));
+          }}
+        >
+          {[10, 20, 30, 40, 50].map((pageSize) => (
+            <option key={pageSize} value={pageSize}>
+              Show {pageSize}
+            </option>
+          ))}
+        </select>
+      </div>
+    </>
   );
 }
